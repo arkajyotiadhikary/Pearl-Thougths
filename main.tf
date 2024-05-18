e4307d372b4a8c918e6925abc54c2961197f2467
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-05-18 18:24:01 +0530

    terraform fix

--- main.tf
+++ main.tf
@@ -6,6 +6,44 @@ resource "aws_ecr_repository" "my_app" {
   name = "my-app-repo"
 }
 
+# resource "aws_iam_role" "ecs_task_execution" {
+#   name = "pearl-test"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Action = "sts:AssumeRole",
+#       Effect = "Allow",
+#       Principal = {
+#         Service = "ecs-tasks.amazonaws.com"
+#       }
+#     }]
+#   })
+
+#   managed_policy_arns = [
+#     "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+#   ]
+# }
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "pearl-test"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
+}
+
 
 resource "aws_ecs_cluster" "my_app_cluster" {
   name = "my-app-cluster"
