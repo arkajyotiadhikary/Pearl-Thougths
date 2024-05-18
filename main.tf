51147f777e993f970fa5c44a034fdd10b54791d9
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-05-18 14:44:56 +0530

    init

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "ap-south-1"  # Specify your AWS region
+}
+
+resource "aws_ecr_repository" "my_app" {
+  name = "my-app-repo"
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "pearl-test"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
+}
+
+resource "aws_ecs_cluster" "my_app_cluster" {
+  name = "my-app-cluster"
+}
+
+resource "aws_ecs_task_definition" "my_app" {
+  family                   = "my-app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([{
+    name      = "my-app"
+    image     = "arkainit/pearl:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 8000
+      hostPort      = 8000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "my_app" {
+  name            = "my-app-service"
+  cluster         = aws_ecs_cluster.my_app_cluster.id
+  task_definition = aws_ecs_task_definition.my_app.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-0dbcc75f8ab359175"]  # Replace with your subnet IDs
+    security_groups = ["sg-01c0dead894195c58"]  # Replace with your security group ID
+  }
+
+  desired_count = 1
+}
